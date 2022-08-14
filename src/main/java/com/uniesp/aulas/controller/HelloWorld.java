4af3e2453930518096e7c84f8b5bb1e1b0f3e332
Samara Silva <samarasilva.dev@gmail.com>
2022-08-14 18:50:31 -0300

    Código Hello World utilizando o Spring Boot

---
+++ src/main/java/com/uniesp/aulas/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.uniesp.aulas.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorld {
+    @RequestMapping("/")
+    public String helloWorld() {
+        System.out.println("Vai retornar hello world");
+        return "Hello World!";
+    }
+}
